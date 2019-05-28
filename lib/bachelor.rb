cfef374e551ee94d4b3d9080c2d1ecd4120de030
Alexander Riccio <test35965@gmail.com>
2019-05-28 22:01:07 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,6 +45,6 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestant|
     total += contestant["age"].to_f
   end
-  (total.to_i/ (data[season].length) )
+  (total/ (data[season].length) ).to_i
 end
 
