6d76e17203010c98d1d959d9d9b40c6dfbef530b
Alexander Riccio <test35965@gmail.com>
2019-05-28 21:49:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,7 +9,9 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, season_data|
+    
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
