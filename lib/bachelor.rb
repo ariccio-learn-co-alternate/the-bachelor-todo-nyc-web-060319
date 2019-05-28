752b1986290289009f3d929eaefaf7fccd67ad53
Alexander Riccio <test35965@gmail.com>
2019-05-28 21:48:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,8 +2,10 @@ require "pry"
 def get_first_name_of_season_winner(data, season)
   # code here
   season_won_data = data[season]
-  winner_index = season_won_data.find_index( |contestant| contestant["status"] == "winner")
-  binding.pry
+  winner_index = season_won_data.find_index( |contestant| contestant["status"] == "Winner")
+  winner_full_name = season_won_data[winner_index]["name"]
+  names = winner_full_name.split(" ")
+  names[0]
 end
 
 def get_contestant_name(data, occupation)
